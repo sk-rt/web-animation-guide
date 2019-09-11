e399f72ef5caf61de86ccd87a0ce1ce33bad0dc4
Ryuta Sakai <info@sakairyuta.com>
2019-09-11 21:06:51 +0900

    ナビアップデート

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -49,9 +49,9 @@ class Navigation extends React.Component {
                         );
                       })}
                     </div>
-                    <Link className={`c-posts-nav__static-page`} to={`/documents/`}>
+                    {/* <Link className={`c-posts-nav__static-page`} to={`/documents/`}>
                       2019 素材置き場
-                    </Link>
+                    </Link> */}
                   </nav>
                 </div>
               );
